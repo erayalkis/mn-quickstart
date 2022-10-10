fa8f424ee7ef5df3560ac265e3c89ec43080a54e
Eray Alkış <lolpo12345@outlook.com>
2022-10-11 00:49:45 +0300

    Use service

--- src/main/java/com/erayalkis/udemy/HelloWorldController.java
+++ src/main/java/com/erayalkis/udemy/HelloWorldController.java
@@ -3,12 +3,16 @@ package com.erayalkis.udemy;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import jakarta.inject.Inject;
 
 @Controller("/hello")
 public class HelloWorldController {
-  
+
+  @Inject
+  private HelloWorldService service;
+
   @Get(produces = MediaType.TEXT_PLAIN)
   public String index() {
-    return "Hello World!";
-  } 
+    return service.helloFromService();
+  }
 }
