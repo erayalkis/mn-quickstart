0f1b8c84c522383f7677f22576babdade6969760
Eray Alkış <lolpo12345@outlook.com>
2022-10-11 00:52:13 +0300

    Use constructor based injection in controller

--- src/main/java/com/erayalkis/udemy/HelloWorldController.java
+++ src/main/java/com/erayalkis/udemy/HelloWorldController.java
@@ -3,13 +3,15 @@ package com.erayalkis.udemy;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import jakarta.inject.Inject;
 
 @Controller("/hello")
 public class HelloWorldController {
 
-  @Inject
-  private HelloWorldService service;
+  private final HelloWorldService service;
+
+  public HelloWorldController(HelloWorldService service) {
+    this.service = service;
+  }
 
   @Get(produces = MediaType.TEXT_PLAIN)
   public String index() {
