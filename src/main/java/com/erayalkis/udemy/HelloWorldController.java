1770f7b612038ca74d3d7241d5aeea8ec26fb615
Eray Alkış <lolpo12345@outlook.com>
2022-10-11 01:21:29 +0300

    Set up logger

--- src/main/java/com/erayalkis/udemy/HelloWorldController.java
+++ src/main/java/com/erayalkis/udemy/HelloWorldController.java
@@ -1,5 +1,8 @@
 package com.erayalkis.udemy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
@@ -7,6 +10,7 @@ import io.micronaut.http.annotation.Get;
 @Controller("/hello")
 public class HelloWorldController {
 
+  private static final Logger LOG = LoggerFactory.getLogger(HelloWorldController.class);
   private final MyService service;
 
   public HelloWorldController(MyService service) {
@@ -15,6 +19,7 @@ public class HelloWorldController {
 
   @Get(produces = MediaType.TEXT_PLAIN)
   public String index() {
+    LOG.debug("Called Hello World API");
     return service.helloFromService();
   }
 }
